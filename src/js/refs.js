596491aacbd7db8a4b07cc0cbf8fac23e46ef09e
MarcinNieradka <nieradka.marcin@gmail.com>
2023-05-18 22:33:57 +0200

    template-of-pagination

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,5 @@
 export const refs = {
   galleryEl: document.getElementById('gallery'),
   searchEl: document.getElementById('search-box'),
+  observerGalleryEl: document.getElementById('observer-gallery'),
 };
