563bf015b854ed442a432d81eb0d51c681e49f17
Anastazja Nikola Bokun <anastazjabokun@tlen.pl>
2023-05-23 01:43:14 +0200

    zmiany w modalu plus gotowy modal
    pozostaje jeszcze zrobić tak by modal zamykał się gdy kliknie się na background ale to już później bo padam

--- src/js/renderMovies.js
+++ src/js/renderMovies.js
@@ -7,6 +7,7 @@ const IMG_URL = 'https://image.tmdb.org/t/p/w300';
 
 const idToGenre = genresArray => {
   let genresList = '';
+<<<<<<< Updated upstream
   genresArray.forEach(
     id =>
       (genresList += `${
@@ -16,6 +17,10 @@ const idToGenre = genresArray => {
         })[0].name
       }, `),
   );
+=======
+  genresArray.forEach(id => (genresList += `${genres.filter(genre => genre.id === id)[0].name}, `));
+  genresList = genresList.slice(0, -2);
+>>>>>>> Stashed changes
   return genresList;
 };
 
@@ -76,7 +81,13 @@ export const renderMovies = dataMovies => {
         <ul class="modal-content__list">
         <div class="modal-content__list-box-1">
         <li>Vote / Votes</li>
+<<<<<<< Updated upstream
         <li class="modal-content__list-result"><span class="text-transform-1">${Math.round(movie.vote_average).toFixed(1)}</span> / ${movie.vote_count}</li>
+=======
+        <li class="modal-content__list-result"><span class="text-transform-1">${Math.round(
+          movie.vote_average,
+        ).toFixed(1)}</span> / <span class="text-transform-2"> ${movie.vote_count}</span></li>
+>>>>>>> Stashed changes
         </div>
         <div class="modal-content__list-box-2">
         <li>Popularity</li>
@@ -84,7 +95,13 @@ export const renderMovies = dataMovies => {
         </div>
         <div class="modal-content__list-box-3">
         <li class="text-transform-li">Original Title</li>
+<<<<<<< Updated upstream
         <li class="modal-content__list-result"><span class="text-transform-2">${movie.original_title}</span></li>
+=======
+        <li class="modal-content__list-result"><span class="text-transform-3">${
+          movie.original_title
+        }</span></li>
+>>>>>>> Stashed changes
         </div>
         <div class="modal-content__list-box-4">
         <li>Genre</li>
@@ -98,6 +115,7 @@ export const renderMovies = dataMovies => {
       <button type="button" class="button-modal__queue">add to queue</button>
       </div>
       </div>`;
+<<<<<<< Updated upstream
       modal.classList.toggle("is-hidden-modal");
   const buttonModal = document.querySelector(".button-modal-close");
   const buttonModalClick = (event) => {
@@ -105,5 +123,36 @@ export const renderMovies = dataMovies => {
     modal.classList.toggle("is-hidden-modal");
   }
   buttonModal.addEventListener("click", buttonModalClick);
+=======
+       modal.classList.remove('is-hidden-modal');
+
+    const buttonModal = document.querySelector('.button-modal-close');
+
+    buttonModal.addEventListener('click', handleEvent);
+    document.addEventListener('keydown', handleEvent);
+
+    let scrollPosition = 0;
+    const body = document.body;
+
+    function disableScroll() {
+      scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+      body.classList.add('lock-scroll', 'lock-scroll-modal-open');
+    }
+
+    function enableScroll() {
+      body.classList.remove('lock-scroll', 'lock-scroll-modal-open');
+      window.scrollTo(0, scrollPosition);
+    }
+
+    function handleEvent(event) {
+      if (event.type === 'click' || (event.type === 'keydown' && event.key === 'Escape')) {
+        modal.innerHTML = '';
+        modal.classList.add('is-hidden-modal');
+        enableScroll();
+      }
+    }
+
+    disableScroll();
+>>>>>>> Stashed changes
   }
 };
