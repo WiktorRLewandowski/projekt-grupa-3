98617bf11d017ebaad2baa1f0430a502bd588eb7
Adrian Miarczyński <adrian331144@gmail.com>
2023-05-22 23:53:29 +0200

    change header on scroll

--- src/js/header.js
+++ src/js/header.js
@@ -6,7 +6,9 @@ window.addEventListener('scroll', () => {
   let header = document.querySelector('.header');
   let containerHeader = document.querySelector('.header__container');
   let wrapper = document.querySelector('.btn__wrapper');
+  let navigation = document.querySelector('.navigation');
 
+  navigation.classList.toggle('navigation__fixed', window.scrollY > 0);
   containerHeader.classList.toggle('header__fixed-container', window.scrollY > 0);
   wrapper.classList.toggle('hidden', window.scrollY > 0);
   header.classList.toggle('header__fixed', window.scrollY > 0);
