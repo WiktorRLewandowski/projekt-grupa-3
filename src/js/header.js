c48f9a9cd7b0c5bad450292fa818f3379eba4cd2
Adrian Miarczyński <adrian331144@gmail.com>
2023-05-24 13:40:24 +0200

    change header

--- src/js/header.js
+++ src/js/header.js
@@ -7,11 +7,13 @@ window.addEventListener('scroll', () => {
   let containerHeader = document.querySelector('.header__container');
   let wrapper = document.querySelector('.btn__wrapper');
   let navigation = document.querySelector('.navigation');
+  let headerWrapper = document.querySelector('.header-wrapper');
 
-  navigation.classList.toggle('navigation__fixed', window.scrollY > 0);
-  containerHeader.classList.toggle('header__fixed-container', window.scrollY > 0);
-  wrapper.classList.toggle('hidden', window.scrollY > 0);
   header.classList.toggle('header__fixed', window.scrollY > 0);
+  containerHeader.classList.toggle('header__fixed-container', window.scrollY > 0);
+  navigation.classList.toggle('navigation__fixed', window.scrollY > 0);
+  wrapper.classList.toggle('hidden', window.scrollY > 0);
+  headerWrapper.classList.toggle('header-wrapper__fixed', window.scrollY > 0);
 });
 
 refs.formEl.addEventListener('submit', ev => {
